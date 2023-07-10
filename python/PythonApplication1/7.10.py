41215ddb29b9aef2e2e858d6e7c27cb08c097b38
m30475 <karrgen@gmail.com>
2023-07-10 23:25:32 +0900

    print 실습

---
+++ python/PythonApplication1/7.10.py
@@ -0,0 +1,7 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print('Hello World')
+Hello World
+>>> print('아시발 회사 좆같다')
+아시발 회사 좆같다
+>>> 
