3e6138d754bdece4e067f99e4c1f56331f4f555f
m30475 <karrgen@gmail.com>
2023-07-12 12:22:20 +0900

    점프투파이선 문자열직전까지 실습 문자열에서 multistring에서 막힘

---
+++ python/PythonApplication1/7.12 연습.py
@@ -0,0 +1,59 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print('아 출근하기 싫다 시발 진짜 ㅠㅠ')
+아 출근하기 싫다 시발 진짜 ㅠㅠ
+>>> a = 4
+>>> b = 8
+>>> print(a**b)
+65536
+>>> food = "python's favorite food is pearl"
+>>> food
+"python's favorite food is pearl"
+>>> food for all
+SyntaxError: invalid syntax
+>>> food = 'python \'s favorite food is perl'
+>>> say = "\"python is very easy.\" he says."
+>>> say
+'"python is very easy." he says.'
+>>> food
+"python 's favorite food is perl"
+
+>>> print('Hello World')
+Hello World
+>>> food = 'Betty\'s favorite food is banana'
+>>> food
+"Betty's favorite food is banana"
+>>> say = "\"역슬래시의 의미는 \'다음 따옴표를 중복사용하게 해준다\'인가?\ 그가 말했다."
+>>> say
+'"역슬래시의 의미는 \'다음 따옴표를 중복사용하게 해준다\'인가?\\ 그가 말했다.'
+>>> say="\"역슬래시의 의미는 \"다음 쌍따옴표를 중복사용하게 해준다.\"인가?\" 그가 말했다."
+>>> say
+'"역슬래시의 의미는 "다음 쌍따옴표를 중복사용하게 해준다."인가?" 그가 말했다.'
+>>> multiline='''
+    life is too short
+    you need python
+    '''
+>>> print(multiline)
+
+    life is too short
+    you need python
+    
+>>> multiline= "life is too short\nYou need python"
+>>> print(multiline)
+life is too short
+You need python
+>>> head = "Python"
+>>> tail = "is fun"
+>>> head+tail
+'Pythonis fun'
+>>> head + tail
+'Pythonis fun'
+>>> head = "Python"
+>>> tail = " is fun"
+>>> head+tail
+'Python is fun'
+>>> head+tail*2
+'Python is fun is fun'
+>>> (head+tail)*2
+'Python is funPython is fun'
+>>> 
